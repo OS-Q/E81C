8992c034ca10849a485535b18235b0687d777af3
qitas <qitas@qitas.cn>
2018-09-28 13:47:50 +0800

    add ip

--- mqtt/iptoyun.py
+++ mqtt/iptoyun.py
@@ -1,12 +1,24 @@
 # import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time
+import socket
+import fcntl
+import struct
 
 HOST= "120.79.63.76"
 PORT= 1883
 
 SID= "/ssh/ip"
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', bytes(ifname[:15], 'utf-8'))
+    )[20:24])
+hostname=socket.getfqdn(socket.gethostname())
+hostip=get_ip_address('wlan0')
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -15,13 +27,12 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+msg.payload.decode("utf-8"))
 
-def ip2yun():  
+def ip2yun(): 
     while True:
         client_id = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
-        ipdata = "120.79.63.76"
-        publish.single(SID, ipdata, qos = 1,hostname=HOST,port=PORT, client_id=client_id,auth = {'username':"ssh", 'password':"555555"})
-       print("ip send")
-       time.sleep(30)
+        ipdata=hostname+"@"+hostip
+        publish.single(SID, ipdata, qos = 1,hostname=HOST,port=PORT, client_id=client_id,auth = {'username':"qitas", 'password':"1234"})
+        time.sleep(60)
 
 if __name__ == '__main__':   
     ip2yun()
