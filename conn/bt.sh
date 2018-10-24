29f278194d9de7b364512ebe504087adea92ca88
qitas <qitas@hotmail.com>
2018-10-24 17:19:30 +0800

    add bt.sh

--- conn/bt.sh
+++ conn/bt.sh
@@ -1,13 +1,12 @@
-#
-# add bluetooth name
+
 sudo mv /etc/machine-info /etc/machine-info.bak
 sudo touch /etc/machine-info
 sudo echo "PRETTY_HOSTNAME=QITAS@192.168.1.77" >> /etc/machine-info
-
+cat /etc/machine-info
 sudo systemctl status bluetooth
-sudo /etc/init.d/bluetooth restart 
+sudo /etc/init.d/bluetooth restart
 
 # scan on
-sudo bluetoothctl 
+sudo bluetoothctl
 devices
 pair 48:2C:A0:37:6B:16
